8dc6dd4b7c645be3b4f3d49d4857b6240802eb53
JuhyunLim <87933367+userJu@users.noreply.github.com>
2021-10-14 17:34:14 +0900

    web APIs upgrade
    dom 수업에서 left, top을 계속 구하는 것보다 transform : translate() 가 성능에 더 좋다는 것을 알게 된 후 성능을 신경 써서 다시 만들기.

--- web APIs 실전 1/main.js
+++ web APIs 실전 1/main.js
@@ -3,17 +3,17 @@ const tag = document.querySelector('.tag')
 const horozontal = document.querySelector('.horozontal')
 const vertical = document.querySelector('.vertical')
 const target = document.querySelector('.target')
+const targetRect =target.getBoundingClientRect();
+const targetHalfWidth = targetRect.width / 2
+const targetFalfHeight = targetRect.height/ 2
 window.addEventListener('mousemove', (e)=>{
     const x = e.clientX;
     const y = e.clientY;
     tag.innerHTML = `position is ${x} , ${y}`
-    horozontal.style.top = `${y}px`;
-    vertical.style.left = `${x}px`;
-    target.style.left = `${x}px`
-    target.style.top = `${y}px`
-    tag.style.left = `${x}px`
-    tag.style.top = `${y}px`
-
+    horozontal.style.transform = `translateY(${y}px)`;
+    vertical.style.transform= `translateX(${x}px)`;
+    target.style.transform = `translate(${x - targetHalfWidth}px, ${y- targetFalfHeight}px)`
+    tag.style.transform = `translate(${x}px, ${y}px)`
 
 
 })
